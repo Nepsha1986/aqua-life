c898d602604911ec604afdd0ada9e55de2636cb8
Alex Nepsha <nepsha1986@gmail.com>
2024-08-02 22:41:39 +0300

    improved header

--- src/containers/AppHeader/components/LangSwitcher/LangSwitcher.tsx
+++ src/containers/AppHeader/components/LangSwitcher/LangSwitcher.tsx
@@ -56,20 +56,22 @@ const LangSwitcher = () => {
         label={locales[locale].label}
       />
 
-      <ul className={styles.langSwitcher__list}>
-        {Object.keys(locales).map((i) => {
-          if (i === locale) return null;
+      <div className={styles.langSwitcher__dropdown}>
+        <ul className={styles.langSwitcher__list}>
+          {Object.keys(locales).map((i) => {
+            if (i === locale) return null;
 
-          return (
-            <li key={i} className={styles.langSwitcher__item}>
-              <LangSwitcherItem
-                link={`/${i}`}
-                label={locales[i as keyof typeof locales].label}
-              />
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li key={i} className={styles.langSwitcher__item}>
+                <LangSwitcherItem
+                  link={`/${i}`}
+                  label={locales[i as keyof typeof locales].label}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
