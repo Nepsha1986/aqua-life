9c91338389f5678af1e05e9e6518bd4748b0cacd
Alex Nepsha <nepsha1986@gmail.com>
2024-08-02 17:07:53 +0300

    tiny fix

--- src/app/[locale]/(posts)/[slug]/_components/UnverifiedAlert/index.tsx
+++ src/app/[locale]/(posts)/[slug]/_components/UnverifiedAlert/index.tsx
@@ -1,10 +1,13 @@
 import { getPageDictionary } from "@/i18n/server/getPageDictionary";
 import { Alert } from "@/ui";
-import { t } from "@/i18n";
+import { Locale, t } from "@/i18n";
 import { UnverifiedAlertI18n } from "@/i18n/types";
 import Link from "next/link";
 
-export const UnverifiedAlert = async ({ locale }) => {
+interface Props {
+  locale: Locale
+}
+export const UnverifiedAlert = async ({ locale }: Props) => {
   const dictionary = await getPageDictionary<UnverifiedAlertI18n>(
     locale,
     "unverified_alert",
